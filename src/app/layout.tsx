5b7704be4161338352f2f04c364d698c46d5593c
ronit narwani <rnarwani55@gmail.com>
2025-09-08 21:13:46 +0000

    npm i @vercel/analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
+import { Analytics } from "@vercel/analytics/react"
 
 export const metadata: Metadata = {
   title: 'BrandTracker Pro',
@@ -22,6 +23,7 @@ export default function RootLayout({
       <body className="font-body antialiased bg-background">
         {children}
         <Toaster />
+        <Analytics />
         </body>
     </html>
   );
